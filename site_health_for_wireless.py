205bda43a9f4ef99e2645577be0d9fee88f61f45
sushil-bhattacharjee <sushil.bhattacharjee@outlook.com>
2025-01-23 16:08:34 +1100

    Added features to check wirelesshealth@sitename

---
+++ site_health_for_wireless.py
@@ -0,0 +1,23 @@
+import json
+import os
+import requests
+import urllib3
+from rich import print as console_print
+
+# Disable SSL warnings
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+# Get the token
+token = os.getenv('DNAC_AUTH_TOKEN')
+
+# Get the site health
+SITE_HEALTH_URL = "https://sandboxdnac2.cisco.com/dna/intent/api/v1/site-health"
+headers = {'X-Auth-Token': token}
+response = requests.get(SITE_HEALTH_URL, headers=headers, verify=False, timeout=30)  # nosec
+console_print(response.json())
+if response.status_code == 200:
+    site_response = response.json()['response']
+    for site in site_response:
+        console_print('{}: {}'.format(site['siteName'], site['networkHealthWireless']))
+else:
+    console_print(response.status_code, response.text)
