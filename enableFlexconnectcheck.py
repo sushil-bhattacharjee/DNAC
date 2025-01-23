353576ba4f938569beb1957429bbef20a01aa54f
sushil-bhattacharjee <sushil.bhattacharjee@outlook.com>
2025-01-23 15:15:10 +1100

    Fixing identation while adding error loging

--- enableFlexconnectcheck.py
+++ enableFlexconnectcheck.py
@@ -1,1 +1,36 @@
- 
+import requests
+import urllib3
+import sys
+from rich import print as console_print
+
+urllib3.disable_warnings()
+
+def get_dnac_wireless_profiles(token):
+    """Get DNAC wireless profiles"""
+    try:
+        url = "https://sandboxdnac2.cisco.com/dna/intent/api/v1/wireless/profile"
+        headers = {"X-Auth-Token": token}
+        response = requests.get(url, headers=headers, verify=False, timeout=30)  # nosec
+        # return response.json()
+        return response.json()[0]['profileDetails']['ssidDetails'][0]['flexConnect']['enableFlexConnect']
+    except Exception as e:
+        console_print(e)
+        sys.exit(1)
+
+def create_dnac_token():
+    """Create DNAC token"""
+    try:
+        url = "https://sandboxdnac2.cisco.com/dna/system/api/v1/auth/token"
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": "Basic ZGV2bmV0dXNlcjpDaXNjbzEyMyE="
+        }
+        response = requests.post(url, headers=headers, verify=False, timeout=30)  # nosec
+        return response.json()["Token"]
+    except Exception as e:
+        console_print(e)
+        sys.exit(1)
+
+if __name__ == "__main__":
+    token = create_dnac_token()
+    console_print(get_dnac_wireless_profiles(token))
